7d5b3f61c2de57c95ec54269271f044c3eb2b7bb
Darryl Pinto <dazpinto@gmail.com>
2014-11-03 16:25:37 +0000

    grunt-init

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-num-format-textbox
-==================
+# NumFormat Textbox
 
-A plugin for formatting currency, percentages and numbers
+A plugin for formatting currency, percentages and number inputs
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/dazpinto/num-format-textbox/master/dist/num-format-textbox.min.js
+[max]: https://raw.github.com/dazpinto/num-format-textbox/master/dist/num-format-textbox.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/num-format-textbox.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/jquery.num-format-textbox.js
@@ -0,0 +1,33 @@
+/*! NumFormat Textbox - v0.0.0 - 2014-11-03
+* https://github.com/dazpinto/num-format-textbox
+* Copyright (c) 2014 Darryl Pinto; Licensed MIT */
+(function($) {
+
+  // Collection method.
+  $.fn.num_format_textbox = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.num_format_textbox = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.num_format_textbox.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.num_format_textbox.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].num_format_textbox = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
